f6199947cd26f67cf61233e107cbb72eb511fb35
abhiramchandra <abhiramchandra2002@gmail.com>
2024-05-03 02:23:17 -0700

    Your commit message

---
+++ contacts/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Contact
+# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display=('id','first_name','last_name','email','car_title','city','created_date')
+    list_display_links=('id','first_name','last_name')
+    search_fields=('first_name','last_name','email','car_title')
+admin.site.register(Contact,ContactAdmin)
